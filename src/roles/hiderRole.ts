232db8dfc80dd0cb036afbe0bb49609f6f50b55a
Przebor <przebot@gmail.com>
2021-05-27 14:06:20 +0200

    Hider role changes

--- src/roles/hiderRole.ts
+++ src/roles/hiderRole.ts
@@ -1,11 +1,16 @@
+import { LobbyInstance } from "@nodepolus/framework/src/api/lobby";
+import { Player } from "@nodepolus/framework/src/player";
 import { Palette } from "@nodepolus/framework/src/static";
+import { PlayerRole } from "@nodepolus/framework/src/types/enums";
 import { PlayerInstance } from "@polusgg/plugin-polusgg-api/node_modules/@nodepolus/framework/src/api/player";
 import { BaseManager } from "@polusgg/plugin-polusgg-api/src/baseManager/baseManager";
-import { BaseRole } from "@polusgg/plugin-polusgg-api/src/baseRole";
 import { RoleAlignment, RoleMetadata } from "@polusgg/plugin-polusgg-api/src/baseRole/baseRole";
+import { Crewmate } from "@polusgg/plugin-polusgg-api/src/baseRole/crewmate/crewmate";
+import { Services } from "@polusgg/plugin-polusgg-api/src/services";
 import { StartGameScreenData } from "@polusgg/plugin-polusgg-api/src/services/roleManager/roleManagerService";
+import { ServiceType } from "@polusgg/plugin-polusgg-api/src/types/enums";
 
-export class HiderRole extends BaseRole {
+export class HiderRole extends Crewmate {
   protected readonly metadata: RoleMetadata = {
     name: "Hider",
     alignment: RoleAlignment.Crewmate,
@@ -13,6 +18,37 @@ export class HiderRole extends BaseRole {
 
   constructor(owner: PlayerInstance) {
     super(owner);
+    Services.get(ServiceType.RoleManager).setBaseRole(this.owner as Player, PlayerRole.Crewmate);
+    this.onReady();
+  }
+
+  onReady(): void {
+    const roleManager = Services.get(ServiceType.RoleManager);
+
+    this.catch("player.task.completed", e => e.getPlayer()).execute(e => {
+      const player = e.getPlayer();
+      const lobby = player.getLobby();
+
+      if (this.checkAllTasks(lobby)) {
+        roleManager.setEndGameData(player.getSafeConnection(), {
+          title: player.getRole() === PlayerRole.Crewmate ? "Victory" : "Defeat",
+          subtitle: "tazkz finished ez",
+          color: [255, 140, 238, 255],
+          yourTeam: lobby.getPlayers(),
+        });
+      }
+    });
+  }
+
+  checkAllTasks(lobby: LobbyInstance): boolean {
+    lobby.getPlayers()
+      .forEach(player => {
+        if (player.getTasks().filter(x => !x[1]).length > 0) {
+          return false;
+        }
+      });
+
+    return true;
   }
 
   getManagerType(): typeof BaseManager {
