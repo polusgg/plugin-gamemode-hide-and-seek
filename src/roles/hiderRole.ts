db1ffc06997702b987daa420c41a3976cd93acdc
Przebor <przebot@gmail.com>
2021-06-14 18:55:46 +0200

    Removed useless comments in hider and seeker roles

--- src/roles/hiderRole.ts
+++ src/roles/hiderRole.ts
@@ -1,14 +1,9 @@
-// import { LobbyInstance } from "@nodepolus/framework/src/api/lobby";
-// import { Player } from "@nodepolus/framework/src/player";
 import { Palette } from "@nodepolus/framework/src/static";
-// import { PlayerRole } from "@nodepolus/framework/src/types/enums";
 import { PlayerInstance } from "@polusgg/plugin-polusgg-api/node_modules/@nodepolus/framework/src/api/player";
 import { BaseManager } from "@polusgg/plugin-polusgg-api/src/baseManager/baseManager";
 import { RoleAlignment, RoleMetadata } from "@polusgg/plugin-polusgg-api/src/baseRole/baseRole";
 import { Crewmate } from "@polusgg/plugin-polusgg-api/src/baseRole/crewmate/crewmate";
-// import { Services } from "@polusgg/plugin-polusgg-api/src/services";
 import { StartGameScreenData } from "@polusgg/plugin-polusgg-api/src/services/roleManager/roleManagerService";
-// import { ServiceType } from "@polusgg/plugin-polusgg-api/src/types/enums";
 
 export class HiderRole extends Crewmate {
   protected readonly metadata: RoleMetadata = {
@@ -16,40 +11,7 @@ export class HiderRole extends Crewmate {
     alignment: RoleAlignment.Crewmate,
   };
 
-  // constructor(owner: PlayerInstance) {
-  //   super(owner);
-  //   Services.get(ServiceType.RoleManager).setBaseRole(this.owner as Player, PlayerRole.Crewmate);
-  //   this.onReady();
-  // }
-
-  // onReady(): void {
-  //   const roleManager = Services.get(ServiceType.EndGame);
-  //
-  //   this.catch("player.task.completed", e => e.getPlayer()).execute(e => {
-  //     const player = e.getPlayer();
-  //     const lobby = player.getLobby();
-  //
-  //     if (this.checkAllTasks(lobby)) {
-  //       roleManager.setEndGameData(player.getSafeConnection(), {
-  //         title: player.getRole() === PlayerRole.Crewmate ? "Victory" : "Defeat",
-  //         subtitle: "tazkz finished ez",
-  //         color: [255, 140, 238, 255],
-  //         yourTeam: lobby.getPlayers(),
-  //       });
-  //     }
-  //   });
-  // }
-  //
-  // checkAllTasks(lobby: LobbyInstance): boolean {
-  //   lobby.getPlayers()
-  //     .forEach(player => {
-  //       if (player.getTasks().filter(x => !x[1]).length > 0) {
-  //         return false;
-  //       }
-  //     });
-  //
-  //   return true;
-  // }
+  // Somehow implement hider being transparent while standing still
 
   getManagerType(): typeof BaseManager {
     return class extends BaseManager {
--- src/roles/seekerRole.ts
+++ src/roles/seekerRole.ts
@@ -1,13 +1,9 @@
 import { Palette } from "@nodepolus/framework/src/static";
 import { PlayerInstance } from "@polusgg/plugin-polusgg-api/node_modules/@nodepolus/framework/src/api/player";
-// import { Player } from "@nodepolus/framework/src/player";
-// import { PlayerRole } from "@polusgg/plugin-polusgg-api/node_modules/@nodepolus/framework/src/types/enums/playerRole";
 import { BaseManager } from "@polusgg/plugin-polusgg-api/src/baseManager/baseManager";
 import { RoleAlignment, RoleMetadata } from "@polusgg/plugin-polusgg-api/src/baseRole/baseRole";
 import { Impostor } from "@polusgg/plugin-polusgg-api/src/baseRole/impostor/impostor";
-// import { Services } from "@polusgg/plugin-polusgg-api/src/services";
 import { StartGameScreenData } from "@polusgg/plugin-polusgg-api/src/services/roleManager/roleManagerService";
-// import { ServiceType } from "@polusgg/plugin-polusgg-api/src/types/enums";
 
 export class SeekerRole extends Impostor {
   protected readonly metadata: RoleMetadata = {
@@ -22,33 +18,8 @@ export class SeekerRole extends Impostor {
       e.cancel();
     });
 
-    this.catch("meeting.started", e => e.getCaller()).execute(e => {
-      e.cancel();
     });
-  }
 
-  // onReady(): void {
-  //   //const bodyService = Services.get(ServiceType.DeadBody);
-  //   /*this.catch("player.position.updated", e => e.getPlayer()).execute(event => {
-  //     console.log(event.getPlayer().getLobby().getAge());
-  //
-  //     if (event.getPlayer().getLobby().getAge() <= 5) {
-  //       console.log("a");
-  //       event.cancel();
-  //     }
-  //   });*/
-  //
-  //   this.catch("room.sabotaged", e => e.getPlayer()).execute(e => {
-  //     e.cancel();
-  //   });
-  //
-  //   this.catch("meeting.started", e => e.getCaller()).execute(e => {
-  //     e.cancel();
-  //   });
-  //
-  //   /*this.catch("player.murdered", e => e.getPlayer()).execute(e => {
-  //   });*/
-  // }
 
   getManagerType(): typeof BaseManager {
     return class extends BaseManager {
