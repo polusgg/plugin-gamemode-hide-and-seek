9fefca56e283f416973f567d7d769c4e3a056f2f
Przebor <przebot@gmail.com>
2021-06-19 10:03:25 +0200

    Implement hider opacity calculation based on velocity

--- src/roles/hiderRole.ts
+++ src/roles/hiderRole.ts
@@ -1,9 +1,12 @@
 import { Palette } from "@nodepolus/framework/src/static";
+import { clamp } from "@nodepolus/framework/src/util/functions";
 import { PlayerInstance } from "@polusgg/plugin-polusgg-api/node_modules/@nodepolus/framework/src/api/player";
 import { BaseManager } from "@polusgg/plugin-polusgg-api/src/baseManager/baseManager";
 import { RoleAlignment, RoleMetadata } from "@polusgg/plugin-polusgg-api/src/baseRole/baseRole";
 import { Crewmate } from "@polusgg/plugin-polusgg-api/src/baseRole/crewmate/crewmate";
+import { Services } from "@polusgg/plugin-polusgg-api/src/services";
 import { StartGameScreenData } from "@polusgg/plugin-polusgg-api/src/services/roleManager/roleManagerService";
+import { ServiceType } from "@polusgg/plugin-polusgg-api/src/types/enums";
 
 export class HiderRole extends Crewmate {
   protected readonly metadata: RoleMetadata = {
@@ -11,7 +14,25 @@ export class HiderRole extends Crewmate {
     alignment: RoleAlignment.Crewmate,
   };
 
-  // Somehow implement hider being transparent while standing still
+  constructor(owner: PlayerInstance) {
+    super(owner);
+
+    const playerAnimationService = Services.get(ServiceType.Animation);
+
+    this.catch("player.position.walked", e => e.getPlayer()).execute(event => {
+      if (event.getPlayer().isDead()) {
+        return;
+      }
+
+      const opacity = clamp(event.getNewVelocity().magnitude() / event.getPlayer().getLobby().getOptions()
+        .getPlayerSpeedModifier(), 0.2, 0.7);
+
+      if (opacity !== event.getPlayer().getMeta("hns.currentopacity")) {
+        event.getPlayer().setMeta("hns.currentopacity", opacity);
+        playerAnimationService.setOpacity(event.getPlayer(), opacity);
+      }
+    });
+  }
 
   getManagerType(): typeof BaseManager {
     return class extends BaseManager {
