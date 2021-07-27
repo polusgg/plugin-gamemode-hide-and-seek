2bee4b2c8193c24d330c0cf9506c686da4f049b7
Jan Przebor <przebot@gmail.com>
2021-07-27 19:19:27 +0200

    Make opacity transition smoooooth

--- src/roles/hiderRole.ts
+++ src/roles/hiderRole.ts
@@ -5,8 +5,10 @@ import { BaseManager } from "@polusgg/plugin-polusgg-api/src/baseManager/baseMan
 import { RoleAlignment, RoleMetadata } from "@polusgg/plugin-polusgg-api/src/baseRole/baseRole";
 import { Crewmate } from "@polusgg/plugin-polusgg-api/src/baseRole/crewmate/crewmate";
 import { Services } from "@polusgg/plugin-polusgg-api/src/services";
+import { PlayerAnimationKeyframe } from "@polusgg/plugin-polusgg-api/src/services/animation/keyframes/player";
 import { StartGameScreenData } from "@polusgg/plugin-polusgg-api/src/services/roleManager/roleManagerService";
 import { ServiceType } from "@polusgg/plugin-polusgg-api/src/types/enums";
+import { PlayerAnimationField } from "@polusgg/plugin-polusgg-api/src/types/playerAnimationFields";
 
 export class HiderRole extends Crewmate {
   protected readonly metadata: RoleMetadata = {
@@ -19,17 +21,24 @@ export class HiderRole extends Crewmate {
 
     const playerAnimationService = Services.get(ServiceType.Animation);
 
-    this.catch("player.position.walked", e => e.getPlayer()).execute(event => {
+    this.catch("player.position.walked", e => e.getPlayer()).execute(async event => {
       if (event.getPlayer().isDead()) {
         return;
       }
 
       const opacity = clamp(event.getNewVelocity().magnitude() / event.getPlayer().getLobby().getOptions()
-        .getPlayerSpeedModifier(), 0.2, 0.7);
+        .getPlayerSpeedModifier(), 0.12, 0.7);
 
       if (opacity !== event.getPlayer().getMeta("pgg.hns.currentopacity")) {
+        await playerAnimationService.beginPlayerAnimation(event.getPlayer(), [PlayerAnimationField.Opacity, PlayerAnimationField.SkinOpacity, PlayerAnimationField.HatOpacity, PlayerAnimationField.PetOpacity, PlayerAnimationField.Angle], [
+          new PlayerAnimationKeyframe({
+            offset: 0,
+            duration: 100,
+            opacity: opacity,
+            petOpacity: opacity,
+          }),
+        ], false);
         event.getPlayer().setMeta("pgg.hns.currentopacity", opacity);
-        playerAnimationService.setOpacity(event.getPlayer(), opacity);
       }
     });
   }
